d5c9a2a56d54907aaccded4d8fe1e54c986ec800
moraleslakson-lab <moraleslakson@gmail.com>
2026-08-29 13:32:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,16 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/crown-copycat/",
   plugins: [
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
     tanstackStart(),
     react(),
   ],
